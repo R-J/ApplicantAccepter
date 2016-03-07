e9973d6bbfcad1a291af130c3471c121fd1b1703
R-J <root@localhost>
2016-03-07 18:03:35 +0100

    Minor coding standard updates

--- class.applicantaccepter.plugin.php
+++ class.applicantaccepter.plugin.php
@@ -1,10 +1,10 @@
-<?php defined('APPLICATION') or die;
+<?php
 
 $PluginInfo['ApplicantAccepter'] = array(
     'Name' => 'Applicant Accepter',
     'Description' => 'Adds information to user table who accepted an applicant.',
-    'Version' => '0.1',
-    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'Version' => '0.2',
+    'RequiredApplications' => array('Vanilla' => '2.2'),
     'SettingsUrl' => '/dashboard/settings/applicantaccepter',
     'SettingsPermission' => 'Garden.Settings.Manage',
     'Author' => 'Robin Jurinka',
@@ -19,113 +19,117 @@ class ApplicantAccepterPlugin extends Gdn_Plugin {
     // called when plugin is enabled
     public function setup() {
         // init config settings
-        if (!C('Plugins.ApplicantAccepter.ApplicantRoleID')) {
-            SaveToConfig('Plugins.ApplicantAccepter.ApplicantRoleID', 4);
-        }
-        if (!C('Plugins.ApplicantAccepter.ShowDiscoveryText')) {
-            SaveToConfig('Plugins.ApplicantAccepter.ShowDiscoveryText', true);
-        }
-    
+        touchConfig('Plugins.ApplicantAccepter.ApplicantRoleID', 4);
+        touchConfig('Plugins.ApplicantAccepter.ShowDiscoveryText', true);
+
         // change user table
         $this->structure();
     }
 
     // called by setup, adds column to user table
     public function structure() {
-        $Database = Gdn::Database();
-        $Structure = $Database->Structure();
-        $Structure
-            ->Table('User')
-            ->Column('ApplicantAccepter', 'int(11)', 0)
-            ->Set();
+        Gdn::database()->structure()
+            ->table('User')
+            ->column('ApplicantAccepter', 'int(11)', 0)
+            ->set();
     }
     
     // very basic settings screen
-    public function settingsController_applicantAccepter_create ($Sender) {
-        $Sender->Permission('Garden.Settings.Manage');
-        $Sender->SetData('Title', T('Applicant Accepter Settings'));
-        $Sender->AddSideMenu('dashboard/settings/plugins');
-        $Conf = new ConfigurationModule($Sender);
-        $Conf->Initialize(array(
+    public function settingsController_applicantAccepter_create($sender) {
+        $sender->permission('Garden.Settings.Manage');
+        $sender->setData('Title', t('Applicant Accepter Settings'));
+        $sender->addSideMenu('dashboard/settings/plugins');
+        $configurationModule = new configurationModule($sender);
+        $configurationModule->initialize(array(
             'Plugins.ApplicantAccepter.ApplicantRoleID',
             'Plugins.ApplicantAccepter.ShowDiscoveryText'
         ));
-        $Conf->RenderAll();
-    } 
-    
-    // store the current user to accepted applicants
-    public function userController_afterApproveUser_handler ($Sender) {
-        $InsertUserID = Gdn::Session()->UserID;
-        Gdn::UserModel()->SetField($Sender->EventArguments['UserID'], 'ApplicantAccepter', $InsertUserID);
+        $configurationModule->renderAll();
     }
-    
+
+    // store the current user to accepted applicants
+    public function userController_afterApproveUser_handler($sender, $args) {
+        Gdn::userModel()->setField(
+            $args['UserID'],
+            'ApplicantAccepter',
+            Gdn::session()->UserID
+        );
+    }
+
     // if accepting is done by changing role
-    public function userModel_beforeSave_handler ($Sender) {
-        $InsertUserID = Gdn::Session()->UserID;
-        $User = $Sender->EventArguments['LoadedUser'];
-        $UserID = $User->UserID;
+    public function userModel_beforeSave_handler($sender, $args) {
+        $insertUserID = Gdn::session()->UserID;
+        $user = $args['LoadedUser'];
+        $userID = $user->UserID;
 
         // we are not interested in users changing their own profile
-        if ($InsertUserID == $UserID) {
+        if ($insertUserID == $userID) {
             return;
         }
 
         // if roles are not changed, we have nothing to do
-        $FormPostValues = $Sender->EventArguments['FormPostValues'];
-        if (!isset($FormPostValues['RoleID'])) {
+        $formPostValues = $args['FormPostValues'];
+        if (!isset($formPostValues['RoleID'])) {
             return;
         }
-        
+
         // if user is no Applicant we have nothing to do
-        $ApplicantRoleID = C('Plugins.ApplicantAccepter.ApplicantRoleID', 4);
-        $UserRoles = ConsolidateArrayValuesByKey(Gdn::UserModel()->GetRoles($UserID), 'RoleID');
-        if (!in_array($ApplicantRoleID, $UserRoles)) {
+        $applicantRoleID = c('Plugins.ApplicantAccepter.ApplicantRoleID', 4);
+        $userRoles = array_column(Gdn::userModel()->getRoles($UserID), 'RoleID');
+        if (!in_array($applicantRoleID, $userRoles)) {
             return;
         }
 
         // is Applicant role still in the roles to be saved? Also not interesting
-        if (in_array($ApplicantRoleID, $FormPostValues['RoleID'])) {
+        if (in_array($applicantRoleID, $formPostValues['RoleID'])) {
             return;
         }
-        
+
         // now that we've excluded everything else we have to keep the inserting user
-        $this->_InsertUserID = $InsertUserID;
+        $this->_InsertUserID = $insertUserID;
     }
 
     // add Applicant accepter info if it has been set before
-    public function userModel_afterSave_handler ($Sender) {
+    public function userModel_afterSave_handler($sender, $args) {
         if ($this->_InsertUserID > 0) {
-            Gdn::UserModel()->SetField($Sender->EventArguments['UserID'], 'ApplicantAccepter', $this->_InsertUserID);
+            Gdn::UserModel()->setField(
+                $args['UserID'],
+                'ApplicantAccepter',
+                $this->_InsertUserID
+            );
         }
     }
-    
+
     // add Applicant accepter to user list
-    public function userController_userCell_handler ($Sender) {
-        if (!isset($Sender->EventArguments['User'])) {
+    public function userController_userCell_handler($sender, $args) {
+        if (!isset($args['User'])) {
             // print out header
-            echo '<th>'.T('Applicant Accepter').'</th>';
-            if (C('Plugins.ApplicantAccepter.ShowDiscoveryText', true) == true) {
-                echo '<th>'.T('Reason for Joining').'</th>';
+            echo '<th>'.t('Applicant Accepter').'</th>';
+            if (c('Plugins.ApplicantAccepter.ShowDiscoveryText', true) == true) {
+                echo '<th>'.t('Reason for Joining').'</th>';
             }
         } else {
             // get the info of the applicant approver
-            $ApplicantAccepterID = $Sender->EventArguments['User']->ApplicantAccepter;
-            $ApplicantAccepterName = '';
-            if ($ApplicantAccepterID > 0) {
-                $ApplicantAccepterName = Gdn::UserModel()->GetID($ApplicantAccepterID)->Name;
+            $applicantAccepterID = $args['User']->ApplicantAccepter;
+            $applicantAccepterName = '';
+            if ($applicantAccepterID > 0) {
+                $applicantAccepterName = Gdn::UserModel()->getID($applicantAccepterID)->Name;
             }
-            echo '<td>'.$ApplicantAccepterName.'</td>';
-            // show reason for joining if it 
-            if (C('Plugins.ApplicantAccepter.ShowDiscoveryText', true) == true) {
-                echo '<td>'.$Sender->EventArguments['User']->DiscoveryText.'</td>';
+            echo '<td>'.$applicantAccepterName.'</td>';
+            // show reason for joining if it
+            if (c('Plugins.ApplicantAccepter.ShowDiscoveryText', true) == true) {
+                echo '<td>'.$args['User']->DiscoveryText.'</td>';
             }
         }
     }
-    
+
     // add possibility to style table
-    public function base_render_before ($Sender) {
-        if ($Sender->MasterView == 'admin') {
-            $Sender->AddCssFile('applicantaccepter.css', 'plugins/ApplicantAccepter');
+    public function base_render_before($sender) {
+        if ($sender->MasterView == 'admin') {
+            $sender->addCssFile(
+                'applicantaccepter.css',
+                'plugins/ApplicantAccepter'
+            );
         }
-    }    
+    }
 }
